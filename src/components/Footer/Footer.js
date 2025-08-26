304c61b22119b45e147ca48c636f8bed67a1ac84
fabriguaglia <fabriguaglia72@gmail.com>
2025-08-25 21:26:43 -0300

    link fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,8 +18,7 @@ function Footer() {
       </div>
       <div className="text-white text-center p-1 derechosreservados" style={{ backgroundColor: '#12442F', color: '#fff' }}>
           Copyright © 2024 Instituto Santa Ana, todos los derechos reservados.
-          https://r6---sn-uxaxjvh5gbxoupo5-x1xll.googlevideo.com/videoplayback?expire=1756157930&ei=ioOsaNkjoffE3w_8-qigAg&ip=176.1.200.13&id=o-ADiB_KRDObj1S3g7fXX-APqF_CJYr89GhwayVG2R_IVk&itag=18&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&rms=au%2Cau&bui=AY1jyLOx1lCWiW6ezuyCTHx23wmimLa4vJrq8xrL1n52S36s1iogZJ7llf4wKRLeEoVP4Mx9mlapOJLQ&spc=l3OVKUEcc0N0MxE9H-MBVDozBm_AIRiuOR8R4-IuWYIEhakVd1iCZKcrR2uLMZxfyTYY-1pt&vprv=1&svpuc=1&mime=video%2Fmp4&ns=ml4VdFSQyPwPQS1pOU9Gn6cQ&rqh=1&gir=yes&clen=27749685&ratebypass=yes&dur=580.591&lmt=1746453607012424&fexp=51331020%2C51552689%2C51565116%2C51565681%2C51580968&c=MWEB&sefc=1&txp=6219224&n=-IwaDo4JzlZpVg&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AJfQdSswRAIgKK2fbgmUVA5AvEJyCmdLAIMwGOExhS3JIGGBZjRm2kYCIGpT5hJoXD1_e68-HWdORLEtb8bOuVviU_oToyF1lrfr&pot=Mnb4mfVudWkxU_inU2D2Vup6NlkptOIQYLSfD3cQXNgUJFdUpn1h-7ynMbwg_B9wqIX-AQs0E9yo_rY4QSLVUuETJi3L9_9Hf44EH3UnWtNMeqFYij3oeCky8fTOqA-UH_rz1I6Q4F7XTbE1Y8vKjn9GIbxny4mb&cms_redirect=yes&met=1756136333,&mh=Qb&mip=186.136.128.89&mm=31&mn=sn-uxaxjvh5gbxoupo5-x1xll&ms=au&mt=1756135782&mv=m&mvi=6&pl=24&lsparams=met,mh,mip,mm,mn,ms,mv,mvi,pl,rms&lsig=APaTxxMwRgIhAIUNjEqRGkstqF-eedV_t1Zk_I-TDCInk3-L1iHzR9dPAiEA1x2kk1_VVoCTdi4gLD3xqvh06ZHQpljA9V8hDZWtXC8%3D
-        </div>
+          </div>
     </footer>
   )
 }
